5943d5945f65ff3137e1dce9b3ecd220a521cb88
Alfonso Muñoz Sánchez <alfonmusan14@gmail.com>
2019-02-05 18:53:14 +0100

    puesta a punto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCnGoIITRCf2PLwjg7cSXFilbZuOXHqrGc',
+    authDomain: 'todolist-609d1.firebaseapp.com',
+    databaseURL: 'https://todolist-609d1.firebaseio.com',
+    projectId: 'todolist-609d1',
+    storageBucket: 'todolist-609d1.appspot.com',
+    messagingSenderId: '208388867428'
+  }
 };
